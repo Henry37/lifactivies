982fb9192af1282473d11bc3d804f91b5b212f2a
Henry Sun <Henry37@users.noreply.github.com>
2017-07-11 20:53:46 +0800

    INIT 001 Birth of lifactivities 1.add grunt plugins for development 2.create original files for the structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,28 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        meta: {  
+            srcPath: 'src/sass/', 
+            deployPath: 'build/css/' 
+        },
+
+        replace: {
+            html: {
+                src: ['src/html/index.html'],
+                dest: 'build/',
+                replacements: [
+                    {
+                        from: 'script.js',
+                        to: '<%=pkg.name%>-<%=pkg.version%>.min.js'
+                    },
+                    {
+                        from: 'style.css',
+                        to: '<%=pkg.name%>-<%=pkg.version%>.style.css'
+                    }
+                ]
+            }
+        },
+
         uglify: {
             options: {
                 stripBanners: true,
@@ -9,7 +31,7 @@ module.exports = function (grunt) {
             },
             build: {
                 src: 'src/js/*.js',
-                dest: 'build/<%=pkg.name%>-<%=pkg.version%>.js.min.js'
+                dest: 'build/js/<%=pkg.name%>-<%=pkg.version%>.min.js'
             }
         },
 
@@ -18,10 +40,48 @@ module.exports = function (grunt) {
             options: {
                 jshintrc: '.jshintrc'
             }
+        },
+
+        copy: {
+            img:{
+                expand: true,
+                cwd: 'src/img/',
+                src: '*',
+                dest: 'build/img/'
+            },
+            html:{
+                expand: true,
+                cwd: 'src/html/',
+                src: '*',
+                dest: 'build/'
+            }
+        },
+
+        sass: { 
+            dist: { 
+                files: { '<%= meta.deployPath %><%=pkg.name%>-<%=pkg.version%>.style.css': '<%= meta.srcPath %>style.scss' }, 
+                options: { 
+                    sourcemap: 'none' ,
+                    style:"compressed"
+                } 
+            } 
+        }, 
+        
+        watch: { 
+            scripts: { 
+                files: [ '<%= meta.srcPath %>/**/*.scss' ], 
+                tasks: ['sass'] 
+            } 
         }
 
     });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-text-replace');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['jshint', 'uglify','sass', 'copy', 'replace']);
 };
