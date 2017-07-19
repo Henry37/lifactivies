1f959c2fb9bcd929e7c673c872b09496c5873ec9
Henry Sun <Henry37@users.noreply.github.com>
2017-07-19 23:53:50 +0800

    INIT 001 Birth of lifactivities 1.update the textbox's style & behaviour.

--- src/js/widgets/textbox.js
+++ src/js/widgets/textbox.js
@@ -7,12 +7,28 @@
     $.fac.widget.init({
         "textbox" : function(method){
             var self = this;
-            if(typeof method === "object" && typeof method !== "string"){
+            if(typeof method === "object" && typeof method !== "string" && !self._init){
+                //init widget
                 self.extend(self, method);
-            }
+                var date = new Date();
+                self.id = self.id || date.getTime().toString();
+                self.name = self.name || self.id;
+                self.cls = self.cls || "";
+                self.labelCls = self.labelCls || "";
+                self.inputCls = self.inputCls || "";
 
-            if(!self._init){
-                self.html("<a id='"+ self.id +"' href='https://baidu.com'>click</a>");
+
+                self.addClass("widget-textbox-container"); 
+                self.addClass(self.cls);
+
+                var inHTML = "";
+                inHTML += "<label class='widget-textbox-fieldLabel " + self.labelCls + "' for='" + self.id + "'>" + self.fieldLabel + "</label>";
+                inHTML += "<input class='widget-textbox-input " + self.inputCls + "' id='" + self.id + "'  name='" + self.name + "'/>";
+                
+
+                self.html(inHTML);
+
+
                 self._init = true;
             }
             self.extend(self, {
